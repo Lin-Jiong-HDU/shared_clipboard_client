1e772e996f266b05236282d0e81fbe4b0a13d289
Lin-Jiong-HDU <linjiong2020@outlook.com>
2025-09-06 19:29:34 +0800

    feat: 完成api.py

--- client/api.py
+++ client/api.py
@@ -32,6 +32,48 @@ def get_devices_count():
     response = requests.get(f"{API_URL}/shared_clipboard", headers=headers)
     return response.json()['data']['device_count']
 
+def set_clipboard_content():
+    data = {
+        "request_id": str(uuid4()),
+        "devices_id": clipboard_instance.device_id,
+        "timestamp": datatime.now().isoformat(),
+        "data": {"content": clipboard_instance.get_clipboard_content()}
+    }
+
+    headers = {
+        'accept': 'application/json',
+        'Content-Type': 'application/json'
+    }
+
+    response = requests.post(f"{API_URL}/shared_clipboard/set", json=data, headers=headers)
+    return response.json()
+
+def set_clipboard_content_for_all_devices():
+    data = {
+        "request_id": str(uuid4()),
+        "devices_id": "",
+        "timestamp": datatime.now().isoformat(),
+        "data": {"content": clipboard_instance.get_clipboard_content()}
+    }
+
+    headers = {
+        'accept': 'application/json',
+        'Content-Type': 'application/json'
+    }
+
+    response = requests.post(f"{API_URL}/shared_clipboard/set", json=data, headers=headers)
+    return response.json()
+
+def sync():
+    headers = {
+        'accept': 'application/json'
+    }
+    response = requests.get(f"{API_URL}/shared_clipboard/sync", headers=headers)
+    if response.status_code == 200:
+        content = response.json()['data']['content']
+        clipboard_instance.set_clipboard_content(content)
+    return response.json()
+
 def health_check():
     headers = {
         'accept': 'application/json'
--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from client.clipboard import clipboard_instance
-from client.api import create_shared_clipboard, delete_shared_clipboard, get_devices_count, health_check
+from client.api import create_shared_clipboard, delete_shared_clipboard, get_devices_count, health_check, sync, set_clipboard_content_for_all_devices
 
 create_shared_clipboard()
 print("Device ID:", clipboard_instance.device_id)
 print("Current Clipboard Content:", clipboard_instance.get_clipboard_content())
 print(get_devices_count())
+
+print(set_clipboard_content_for_all_devices())
+print(sync())
 response = delete_shared_clipboard()
 print("Delete Response:", response)
 print("Health Check:", health_check())
